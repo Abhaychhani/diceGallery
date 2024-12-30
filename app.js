b9b47578377b9b475c6add8113002ad259a6b76f
Abhay verma <abhaychhani@gmail.com>
2024-12-30 11:08:57 +0530

    removed comment

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-window.addEventListener("load", () => {
-  const dice = document.querySelector(".dice");
+ const dice = document.querySelector(".dice");
   const loader = document.getElementById("loader");
   const imageUrls = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
   let loadedImages = 0;
@@ -18,5 +17,3 @@ window.addEventListener("load", () => {
     });
   }
   setTimeout(loadImages,1500)
-  // loadImages();
-});
